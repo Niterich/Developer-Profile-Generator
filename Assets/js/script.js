f59e5db2bdfda4b5f238c612768e17d37dd4f14f
Niterich <52295078+Niterich@users.noreply.github.com>
2020-01-18 12:28:54 -0700

    started initial ajax call

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,15 @@
 console.log("connected")
-let user = "niterich";
-$.ajax({
-    url: "https://api.github.com/users/" + user,
-    method: "GET"
-}).then(function(res){
-    console.log(res);
+let user;
+
+$("#search-sumbit").on("click", function(){
+    console.log("clicked")
+    user = $("#profile-search").val();
+    console.log(user);
+    event.preventDefault();
+    $.ajax({
+        url: "https://api.github.com/users/" + user,
+        method: "GET"
+    }).then(function(res){
+        console.log(res);
+    })
 })
