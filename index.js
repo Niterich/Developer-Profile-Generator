776b0ded4d6e442cf4601a45be0e0528ec1d323a
Niterich <52295078+Niterich@users.noreply.github.com>
2020-01-24 23:00:28 -0700

    got ajax call working with axios

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const axios = require("axios");
+const generateHtml = require("./generateHTML");
 
 inquirer
     .prompt([
         {
             type: "input",
             message: "What is your GitHub user name?",
-            name: "name"
+            name: "username"
         },
         {
             type: "list",
             message: "What is your favorite color?",
-            name: "fave color",
+            name: "color",
             choices: [
-                "Red",
-                "Blue",
                 "Green",
-                "Orange",
-                "Yellow",
-                "Purple"
+                "Blue",
+                "Pink",
+                "Red"
             ]
         }
     ])
-    .then(function(response) {
-        user = response.name;
-        console.log(user);
-        $.ajax({
-            url: "https://api.github.com/users/" + user,
-            method: "GET"
-        }).then(function(res){
-            console.log(res);
-        })
-        // const filename = response.name.toLowerCase().split(" ").join("") + ".pdf";
-        // fs.appendFile(filename, JSON.stringify(response, null, "\t"), function(err){
-        //     if (err) {
-        //         return console.log(err);
-        //     }
-        //     console.log("success!");
-        // })
+    // .then(function({ color }){
+    //     // generateHTML();
+    // })
+
+    .then(function({ username }) {
+        const queryUrl = `https://api.github.com/users/${username}`;
+        axios
+            .get(queryUrl)
+            .then(function(res) {
+                console.log(res.data.name)
+    //             // fs.writeFile(`${username}.pdf`, JSON.stringify(res), (err, data) => {
+    //             //     if (err){
+    //             //         throw err;
+    //             //     }
+    //             //     console.log("success");
+    //             // })
+            });
     });
+
+    //How do the two js files communicate with each other? how do i reference a function declared in a different js file?
+
+    //once we have user color, how do we use it to generate html in the corresponding color?
+        //how do we parse the data from the axios call to populate on this new html page?
